53a6718cfa78db9578903fef846bc697ca635830
jhamer02 <jhamer02@hotmail.com>
2016-06-24 18:26:44 -0500

    Initial Commit

---
+++ Navigation Controller/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Navigation Controller
+//
+//  Created by Andrea Rosas Pérez on 24/06/16.
+//  Copyright © 2016 jjpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Navigation Controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Navigation Controller
+//
+//  Created by Andrea Rosas Pérez on 24/06/16.
+//  Copyright © 2016 jjpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
